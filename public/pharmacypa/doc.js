7503697cbbbe278e3cc988f917d03a62481695c5
Sandy <sandy.jernigan@gmail.com>
2020-10-06 23:00:15 -0400

    Testing

---
+++ public/pharmacypa/doc.js
@@ -0,0 +1,8 @@
+
+const docs = {
+  header: 'Pharmacy PA Guide',
+  address: 'https://', // website address
+  changes: [ // changes should be an array of strings, these strings will become list items
+    '.',
+  ],
+}
