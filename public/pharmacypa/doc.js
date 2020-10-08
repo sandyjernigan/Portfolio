69301e797d0411b629be55edcec9a23ddb6750ee
Sandy <sandy.jernigan@gmail.com>
2020-10-08 19:39:42 -0400

    Testing

--- public/pharmacypa/doc.js
+++ public/pharmacypa/doc.js
@@ -3,6 +3,6 @@ const docs = {
   header: 'Pharmacy PA Guide',
   address: 'https://', // website address
   changes: [ // changes should be an array of strings, these strings will become list items
-    '.',
+    'Currently working on this resource, I want to make this into a calendar schedule and drug reference tool that can be use for multiple things. NO coonfidetal info be available.',
   ],
 }
